f90a7b78dacc6998906247a461dfd02652ff9b59
ursamajor0714 <ohyou66@gmail.com>
2026-05-14 09:29:17 +0900

    Add vercel.json

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,6 @@
+import dj_database_url
+import os
+
 """
 Django settings for config project.
 
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,10 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'), # Vercel 환경 변수에서 주소를 읽어옴
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
@@ -115,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
